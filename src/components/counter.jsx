f8d9e742f867b06dbd3a8933810204e637329d01
Jake Napper <napperjlg@gmail.com>
2019-07-11 16:46:39 +0100

    increment button increases count by one

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -18,11 +18,20 @@ class Counter extends Component {
     );
   }
 
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         {this.state.tags.length === 0 && <p>Please create a tag!</p>}
         {this.renderTags()}
       </div>
